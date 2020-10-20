b9a563bd78dd14c5bfe7c601f3f9ef544691ac78
Wilson Styres <wstyres@gmail.com>
2020-10-20 16:34:09 -0400

    Initial Commit

---
+++ Mappalachian/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mappalachian
+//
+//  Created by Wilson Styres on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
