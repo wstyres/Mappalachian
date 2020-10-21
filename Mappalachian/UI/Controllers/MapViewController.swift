ee5b99aaeb5e9cd7dd74f5f9519969ac6ee4a28c
Wilson Styres <wstyres@gmail.com>
2020-10-21 13:00:59 -0400

    Determine which building a user is in

--- Mappalachian/UI/Controllers/MapViewController.swift
+++ Mappalachian/UI/Controllers/MapViewController.swift
@@ -15,15 +15,21 @@ class MapViewController: UIViewController {
     @IBOutlet var cameraButtonContainer: UIView!
     @IBOutlet var cameraButton: UIImageView!
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet var buildingLabel: UILabel!
+    @IBOutlet var buildingBlurView: UIVisualEffectView!
     
     let moc = AppDelegate.delegate().persistentContainer.viewContext
     var polyMap: [MKPolygon: Building] = [MKPolygon: Building]()
+    var locationManager: CLLocationManager?
     
     let booneRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 36.214121, longitude: -81.679117), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.008))
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        buildingBlurView.layer.cornerRadius = buildingBlurView.frame.height / 4
+        buildingBlurView.layer.masksToBounds = true
+        
         cameraButtonContainer.layer.cornerRadius = 60 / 2
         cameraButton.image = UIImage(systemName: "camera.fill")
         cameraButton.tintColor = .systemGroupedBackground
@@ -31,6 +37,13 @@ class MapViewController: UIViewController {
         // Configure mapView to zoom into ASU (at about 36.214121,-81.679117)
         mapView.region = booneRegion
         mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.startUpdatingLocation()
         
         addPolygons()
     }
@@ -54,17 +67,6 @@ class MapViewController: UIViewController {
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let touchPoint = touch.location(in: mapView)
-            let location = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-            print ("[\(location.latitude), \(location.longitude)],")
-        }
-    }
-    
-}
-
-extension MapViewController: CLLocationManagerDelegate {
     func buildingForUserLocation(_ location: CLLocationCoordinate2D) -> Building? {
         for polygon in polyMap.keys {
             let polygonRenderer = MKPolygonRenderer(polygon: polygon)
@@ -76,6 +78,29 @@ extension MapViewController: CLLocationManagerDelegate {
             }
         }
         return nil
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchPoint = touch.location(in: mapView)
+            let location = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            print ("[\(location.latitude), \(location.longitude)],")
+        }
+    }
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let mostRecentLocation = locations.last else {
+            return;
+        }
+        
+        if let building = buildingForUserLocation(mostRecentLocation.coordinate) {
+            buildingLabel.text = "Current Location: \(building.name!)"
+        } else {
+            buildingLabel.text = "You are outside"
+        }
     }
 }
 
