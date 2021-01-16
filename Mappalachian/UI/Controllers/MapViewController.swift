cf307f4b9fe3391162858788584ffdf235c057ce
Wilson Styres <wstyres@gmail.com>
2021-01-15 19:09:24 -0500

    Draw the rest of the owl...

--- Mappalachian/UI/Controllers/MapViewController.swift
+++ Mappalachian/UI/Controllers/MapViewController.swift
@@ -41,6 +41,10 @@ extension Unit: FeatureStyle {
         case restroomUnisex = "restroom.unisex"
         case room
         case nonpublic
+        case classroom
+        case office
+        case laboratory
+        case privateLounge = "privatelounge"
     }
     
     func configure(overlayRenderer: MKOverlayPathRenderer) {
@@ -56,6 +60,14 @@ extension Unit: FeatureStyle {
                 overlayRenderer.fillColor = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1.00)
             case .nonpublic:
                 overlayRenderer.fillColor = UIColor(red: 0.82, green: 0.82, blue: 0.82, alpha: 1.00)
+            case .classroom:
+                overlayRenderer.fillColor = UIColor(red: 0.94, green: 0.89, blue: 0.82, alpha: 1.00)
+            case .office:
+                overlayRenderer.fillColor = UIColor(red: 0.78, green: 0.92, blue: 0.79, alpha: 1.00)
+            case .laboratory:
+                overlayRenderer.fillColor = UIColor(red: 0.91, green: 0.91, blue: 0.63, alpha: 1.00)
+            case .privateLounge:
+                overlayRenderer.fillColor = UIColor(red: 1.00, green: 0.76, blue: 0.71, alpha: 1.00)
             }
         } else {
             overlayRenderer.fillColor = UIColor(red: 0.90, green: 0.90, blue: 0.90, alpha: 1.00)
